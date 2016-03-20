2e803bc0569ef989028013f62205eebb71510a78
Andy Postnikov <apostnikov@gmail.com>
2016-03-20 23:59:58 +0200

    Clean-up \Drupal\styleguide\Form\StyleguideForm

--- src/Form/StyleguideForm.php
+++ src/Form/StyleguideForm.php
@@ -1,41 +1,39 @@
 <?php
-/**
- * @file
- * Contains \Drupal\styleguide\Form\StyleguideForm.
- */
 
 namespace Drupal\styleguide\Form;
 
-
-use Drupal\Core\DependencyInjection\ContainerInjectionInterface;
 use Drupal\Core\Extension\ModuleHandlerInterface;
 use Drupal\Core\Form\FormBase;
 use Drupal\Core\Form\FormStateInterface;
 use Drupal\styleguide\GeneratorInterface;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 
-class StyleguideForm extends FormBase implements ContainerInjectionInterface {
+/**
+ * Defines a form builder to display form controls in style guide.
+ */
+class StyleguideForm extends FormBase {
 
   /**
-   * The styleguide generator service.
+   * The styleguide generator.
    *
-   * @var \Drupal\styleguide\Generator
+   * @var \Drupal\styleguide\GeneratorInterface
    */
   protected $generator;
 
   /**
-   * The module handler service.
+   * The module handler.
    *
    * @var ModuleHandlerInterface
    */
   protected $moduleHandler;
 
   /**
-   * Constructs a new StyleguideForm.
+   * Constructs a new StyleguideForm object.
    *
-   * @param \Drupal\styleguide\Generator|GeneratorInterface $styleguide_generator
-   *   The styleguide generator service.
+   * @param \Drupal\styleguide\GeneratorInterface $styleguide_generator
+   *   The styleguide generator.
    * @param ModuleHandlerInterface $module_handler
+   *   The module handler.
    */
   public function __construct(GeneratorInterface $styleguide_generator, ModuleHandlerInterface $module_handler) {
     $this->generator = $styleguide_generator;
@@ -53,197 +51,184 @@ class StyleguideForm extends FormBase implements ContainerInjectionInterface {
   }
 
   /**
-   *
-   * Returns a unique string identifying the form.
-   *
-   * @return string
-   *   The unique string identifying the form.
+   * {@inheritdoc}
    */
   public function getFormId() {
     return 'styleguide_form';
   }
 
   /**
-   * Sample form, showing all elements.
-   *
-   * @param array $form
-   *   An associative array containing the structure of the form.
-   * @param \Drupal\Core\Form\FormStateInterface $form_state
-   *   The current state of the form.
-   * @param array $form_keys
-   *  An array containing the type of elements to return.
-   *
-   * @return array
-   *   The form structure.
+   * {@inheritdoc}
    */
-  public function buildForm(array $form, FormStateInterface $form_state, $form_keys = array()) {
-    $form = array();
-    $options = array();
+  public function buildForm(array $form, FormStateInterface $form_state, $form_keys = []) {
+    // @todo Use element names and defaults from element_info service.
+    $form = [];
+    $options = [];
     $list = $this->generator->wordList();
     foreach ($list as $item) {
       $options[$item] = $item;
     }
-    $form['select'] = array(
+    $form['select'] = [
       '#type' => 'select',
       '#title' => $this->t('Select'),
       '#options' => $options,
       '#description' => $this->generator->sentence(),
-    );
-    $form['checkbox'] = array(
+    ];
+    $form['checkbox'] = [
       '#type' => 'checkbox',
       '#title' => $this->t('Checkbox'),
       '#value' => 1,
       '#default_value' => 1,
       '#description' => $this->generator->sentence(),
-    );
-    $form['checkboxes'] = array(
+    ];
+    $form['checkboxes'] = [
       '#type' => 'checkboxes',
       '#title' => $this->t('Checkboxes'),
       '#options' => $options,
       '#description' => $this->generator->sentence(),
-    );
-    $form['radios'] = array(
+    ];
+    $form['radios'] = [
       '#type' => 'radios',
       '#title' => $this->t('Radios'),
       '#options' => $options,
       '#description' => $this->generator->sentence(),
-    );
-    $form['textfield'] = array(
+    ];
+    $form['textfield'] = [
       '#type' => 'textfield',
       '#title' => $this->t('Textfield'),
       '#default_value' => $this->generator->words(3, 'ucfirst'),
       '#description' => $this->generator->sentence(),
-    );
-    $form['autocomplete'] = array(
+    ];
+    $form['autocomplete'] = [
       '#type' => 'textfield',
       '#title' => $this->t('Autocomplete textfield'),
       '#default_value' => $this->generator->words(),
       '#description' => $this->generator->sentence(),
       '#autocomplete_path' => 'user/autocomplete',
-    );
-    $form['textfield-machine'] = array(
+    ];
+    $form['textfield-machine'] = [
       '#type' => 'textfield',
       '#title' => $this->t('Textfield, with machine name'),
       '#default_value' => $this->generator->words(3, 'ucfirst'),
       '#description' => $this->generator->sentence(),
-    );
-    $form['machine_name'] = array(
+    ];
+    $form['machine_name'] = [
       '#type' => 'machine_name',
       '#title' => $this->t('Machine name'),
-      '#machine_name' => array(
+      '#machine_name' => [
         'exists' => 'styleguide_machine_name_exists',
-        'source' => array('textfield-machine'),
-      ),
+        'source' => ['textfield-machine'],
+      ],
       '#description' => $this->generator->sentence(),
-    );
-    $form['textarea'] = array(
+    ];
+    $form['textarea'] = [
       '#type' => 'textarea',
       '#title' => $this->t('Textarea'),
       '#default_value' => $this->generator->paragraphs(5, TRUE),
       '#description' => $this->generator->sentence(),
-    );
-    $form['date'] = array(
+    ];
+    $form['date'] = [
       '#type' => 'date',
       '#title' => $this->t('Date'),
       '#description' => $this->generator->sentence(),
-    );
-    $form['file'] = array(
+    ];
+    $form['file'] = [
       '#type' => 'file',
       '#title' => $this->t('File'),
       '#description' => $this->generator->sentence(),
-    );
-    $form['managed_file'] = array(
+    ];
+    $form['managed_file'] = [
       '#type' => 'managed_file',
       '#title' => $this->t('Managed file'),
       '#description' => $this->generator->sentence(),
-    );
-    $form['password'] = array(
+    ];
+    $form['password'] = [
       '#type' => 'password',
       '#title' => $this->t('Password'),
       '#default_value' => $this->generator->words(),
       '#description' => $this->generator->sentence(),
-    );
-    $form['password_confirm'] = array(
+    ];
+    $form['password_confirm'] = [
       '#type' => 'password_confirm',
       '#title' => $this->t('Password confirm'),
-    );
-    $form['weight'] = array(
+    ];
+    $form['weight'] = [
       '#type' => 'weight',
       '#title' => $this->t('Weight'),
       '#delta' => 10,
       '#description' => $this->generator->sentence(),
-    );
-    $form['details-close'] = array(
+    ];
+    $form['details-close'] = [
       '#type' => 'details',
       '#title' => $this->t('Details closed'),
       '#open' => FALSE,
       '#description' => $this->generator->sentence(),
-    );
-    $form['details-open'] = array(
+    ];
+    $form['details-open'] = [
       '#type' => 'details',
       '#title' => $this->t('Details open'),
       '#open' => TRUE,
       '#description' => $this->generator->sentence(),
-    );
-    $form['fieldset'] = array(
+    ];
+    $form['fieldset'] = [
       '#type' => 'fieldset',
       '#title' => $this->t('Fieldset'),
       '#description' => $this->generator->sentence(),
-    );
-    $elements = array('fieldset', 'details-close', 'details-open');
+    ];
+    $elements = ['fieldset', 'details-close', 'details-open'];
     $count = 0;
     foreach ($form as $key => $value) {
       if ($value['#type'] != 'fieldset' && $value['#type'] != 'checkbox' && $count < 2) {
         $count++;
         foreach ($elements as $item) {
-          $form[$item][$key . '-' .  $item] = $value;
+          $form[$item][$key . '-' . $item] = $value;
         }
       }
     }
-    $form['vertical_tabs']['elements'] = array(
+    $form['vertical_tabs']['elements'] = [
       '#type' => 'vertical_tabs',
       '#default_tab' => 'fieldset',
-    );
+    ];
     foreach ($elements as $element) {
       $form['vertical_tabs'][$element] = $form[$element];
       $form['vertical_tabs'][$element]['#type'] = 'details';
       $form['vertical_tabs'][$element]['#group'] = 'elements';
     }
-    $form['markup'] = array(
+    $form['markup'] = [
       '#markup' => $this->t('<p><em>Markup</em>: Note that markup does not allow titles or descriptions. Use "item" for those options.</p>') . $this->generator->paragraphs(1, TRUE),
-    );
-    $form['item'] = array(
+    ];
+    $form['item'] = [
       '#type' => 'item',
       '#title' => $this->t('Item'),
       '#markup' => $this->generator->paragraphs(1, TRUE),
       '#description' => $this->generator->sentence(),
-    );
-    $form['image_button'] = array(
+    ];
+    $form['image_button'] = [
       '#type' => 'image_button',
       '#src' => 'core/misc/druplicon.png',
-      '#attributes' => array('height' => 40),
+      '#attributes' => ['height' => 40],
       '#name' => $this->t('Image button'),
-    );
-    $form['submit'] = array(
+    ];
+    $form['submit'] = [
       '#type' => 'submit',
       '#value' => $this->t('Submit'),
-    );
-    $form['button'] = array(
+    ];
+    $form['button'] = [
       '#type' => 'button',
       '#value' => $this->t('Button'),
-    );
+    ];
 
     if ($this->moduleHandler->moduleExists('filter')) {
-      $form['text_format'] = array(
+      $form['text_format'] = [
         '#title' => $this->generator->sentence(),
         '#type' => 'text_format',
         '#default_value' => $this->generator->paragraphs(5, TRUE),
         '#format' => filter_default_format(),
-      );
+      ];
     }
 
     if (!empty($form_keys)) {
-      $items = array();
+      $items = [];
       foreach ($form_keys as $key) {
         if (isset($form[$key])) {
           $items[$key] = $form[$key];
@@ -256,14 +241,10 @@ class StyleguideForm extends FormBase implements ContainerInjectionInterface {
   }
 
   /**
-   * Form submission handler.
-   *
-   * @param array $form
-   *   An associative array containing the structure of the form.
-   * @param \Drupal\Core\Form\FormStateInterface $form_state
-   *   The current state of the form.
+   * {@inheritdoc}
    */
   public function submitForm(array &$form, FormStateInterface $form_state) {
     // TODO: Implement submitForm() method.
   }
+
 }
